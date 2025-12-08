95763cf808f4223aea2d159457bb1390824ab1a1
ShahanasK7 <skotheri@ecloudcontrol.com>
2025-12-08 16:49:35 +0530

    Updated plugins

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+cd /var/www/html || exit 1
+
+echo "Checking for Hello Dolly plugin..."
+if wp --allow-root plugin is-installed hello; then
+    echo "Hello Dolly plugin found. Deleting..."
+    wp --allow-root plugin delete hello || echo "WP-CLI delete command failed"
+
 filename="AppZ-Healthcare-Case-Study-Oct-2020-v2-1"
 if  grep $filename  -q  /var/www/html/wp-content/themes/Avada/header.php ;then
   echo "theme change already exist..."
